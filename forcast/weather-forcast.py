c5cf1370582b3d0c8d3014224c4ae382f921af4f
Judoguy12 <jrwjudo@gmail.com>
2017-07-10 19:08:55 +0100

    added loop and sleep

--- forcast/weather-forcast.py
+++ forcast/weather-forcast.py
@@ -1,79 +1,83 @@
 import pyowm
 from twython import Twython
 import time,datetime
-
-dateString = "%Y/%m/%d %H:%M:%S"
-time = datetime.datetime.now().strftime(dateString)
-
-
-
-from auth import (
-    consumer_key,
-    consumer_secret,
-    access_token,
-    access_token_secret
-)
-
-twitter = Twython(
-        consumer_key,
-    consumer_secret,
-    access_token,
-    access_token_secret
-)
-
-lat = 52.27
-lon = -2.15
-owm = pyowm.OWM('f5f228ca75823de879e024d518f7a8cf')
-observation = owm.weather_at_place("Droitwich,uk")
-w = observation.get_weather()
-sts = w.get_detailed_status()
-wind = w.get_wind()
-temp = w.get_temperature('celsius').get('temp')
-rain = w.get_rain()
-tomorrow = pyowm.timeutils.tomorrow()
-humid = w.get_humidity()
-sunrise = w.get_sunrise_time('iso')
-sunset = w.get_sunset_time('iso')
-pressure = w.get_pressure()
-fc = owm.daily_forecast('Droitwich,uk', limit = 6)
-f = fc.get_forecast()
-print('Sunrise: '+str(sunrise))
-print('Weather: ')
-print(sts)
-print('Wind: ')
-print(wind)
-print('Temperature, Celcius: ')
-print(temp)
-print('Rain: ')
-print(rain)
-print('Humidity: ')
-print(humid)
-print('Pressure: ')
-print(pressure)
-print('Sunset: '+str(sunset))
-print("Forcast: ")
-for weather in f:
-    print(weather.get_reference_time('iso'),weather.get_status())
-
+from time import sleep
+while 1 ==1:
     
-def forcast():
+    dateString = "%Y/%m/%d %H:%M:%S"
+    time = datetime.datetime.now().strftime(dateString)
+
+
+
+    from auth import (
+        consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret
+    )
+
+    twitter = Twython(
+            consumer_key,
+        consumer_secret,
+        access_token,
+        access_token_secret
+    )
+
+    lat = 52.27
+    lon = -2.15
+    owm = pyowm.OWM('f5f228ca75823de879e024d518f7a8cf')
+    observation = owm.weather_at_place("Droitwich,uk")
+    w = observation.get_weather()
+    sts = w.get_detailed_status()
+    wind = w.get_wind()
+    temp = w.get_temperature('celsius').get('temp')
+    rain = w.get_rain()
+    tomorrow = pyowm.timeutils.tomorrow()
+    humid = w.get_humidity()
+    sunrise = w.get_sunrise_time('iso')
+    sunset = w.get_sunset_time('iso')
+    pressure = w.get_pressure()
+    fc = owm.daily_forecast('Droitwich,uk', limit = 6)
+    f = fc.get_forecast()
+    print('Sunrise: '+str(sunrise))
+    print('Weather: ')
+    print(sts)
+    print('Wind: ')
+    print(wind)
+    print('Temperature, Celcius: ')
+    print(temp)
+    print('Rain: ')
+    print(rain)
+    print('Humidity: ')
+    print(humid)
+    print('Pressure: ')
+    print(pressure)
+    print('Sunset: '+str(sunset))
+    print("Forcast: ")
     for weather in f:
         print(weather.get_reference_time('iso'),weather.get_status())
 
+        
+    def forcast():
+        for weather in f:
+            print(weather.get_reference_time('iso'),weather.get_status())
 
 
 
 
-text = "Droitwich's weather: Status:"+str(sts)+"Wind:"+str(wind)+',Temperature:'+str(temp)+',Rain:'+str(rain)+str(time) 
-text2 = 'Humidity and pressure data for Droitwich. Humidity:'+str(humid)+',Pressure:'+str(pressure)+str(time)
-text3 = "Sunrise and sunset times: Sunrise:"+str(sunrise)+',Sunset:'+str(sunset)+str(time)
-text4 = "The 6 day forcast is: " +str(forcast)
 
-message = text
-twitter.update_status(status = message)
+    text = "Droitwich's weather: Status:"+str(sts)+"Wind:"+str(wind)+',Temperature:'+str(temp)+',Rain:'+str(rain)+str(time) 
+    text2 = 'Humidity and pressure data for Droitwich. Humidity:'+str(humid)+',Pressure:'+str(pressure)+str(time)
+    text3 = "Sunrise and sunset times: Sunrise:"+str(sunrise)+',Sunset:'+str(sunset)+str(time)
+    text4 = "The 6 day forcast is: " +str(forcast)
 
-message2 = text2
-twitter.update_status(status = message2)
+    message = text
+    twitter.update_status(status = message)
 
-message3 = text3
-twitter.update_status(status = message3)
+    message2 = text2
+    twitter.update_status(status = message2)
+
+    message3 = text3
+    twitter.update_status(status = message3)
+    sleep(1800)
+
