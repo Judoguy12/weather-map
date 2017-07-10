43c04bd81ef52eee39d1335d83963ae4faf84565
Judoguy12 <jrwjudo@gmail.com>
2017-07-10 18:53:30 +0100

    added new forcasting function using pyowm

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key = 'mWH8c0GHOLkW8tYziAb9Nktmu'
+consumer_secret = 'PXDLvfWv3Omr9enpgKhjTc47ijowemWOsKFRR9doq7U7EaLdg5'
+access_token = 	'878967874492264450-eZI5WkF8uChWmp5vviyQ0Jm9lw09HkX'
+access_token_secret = 	'1rjJY3DAxnFbOjWVMDPRtVIO7NokCW6L9e6wegfn8ZVYx'
